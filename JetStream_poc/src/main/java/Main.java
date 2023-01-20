00aa649e95fe98372e81acc53bf8df7ac567d3d1
janithPriyankara <56631224+janithPriyankara@users.noreply.github.com>
2023-01-20 09:43:55 +0530

    Specified variables

--- JetStream_poc/src/main/java/Main.java
+++ JetStream_poc/src/main/java/Main.java
@@ -10,23 +10,32 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-//import io.nats.client.ConnectionFactory;
-
-
 public class Main {
 
   public static void main(String[] args)
       throws IOException, InterruptedException, JetStreamApiException, IllegalStateException, TimeoutException {
 
-    Connection nc = Nats.connect("nats://local:vvNGFu3HRk3PMjZHsztlsCZ9MoUwTGzR@0.0.0.0:37733");
+    String userName = "local";
+    String password = "vvNGFu3HRk3PMjZHsztlsCZ9MoUwTGzR";
+    String host = "0.0.0.0";
+    String port = "37733";
+    String streamName = "test_stream";
+    String subjectName = "test_subject_1";
+
+
+    int BATCH_SIZE = 100;
+
+    Connection nc = Nats.connect("nats://"+userName+":"+password+ "@"+host+":"+port);
     JetStream js = nc.jetStream();
 
     PullSubscribeOptions pullOptions = PullSubscribeOptions.builder()
 //          .durable("configurator_service_8")  this is optional
-        .stream("test_stream")
+        .stream(streamName)
         .build();
 
-    JetStreamSubscription sub = js.subscribe("test_subject_1", pullOptions);
+    JetStreamSubscription sub = js.subscribe(subjectName, pullOptions);
+    nc.flush(Duration.ofSeconds(1));
+
     List<Message> messages = sub.fetch(10, Duration.ofMillis(1000));
     for (Message m : messages) {
       // process message
